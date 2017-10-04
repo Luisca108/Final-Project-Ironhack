7d1e2373e1f37737a7cb06c15c5a3e5e15032a69
Luisca108 <luiscarlos3106@gmail.com>
2017-10-04 18:23:02 +0200

    API implementation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RouterModule } from '@angular/router';
 import { IsLoggedInService } from './services/isLoggedIn.canactivate.service';
 import {routes} from './routes';
 import { SignupformComponent } from './signupform/signupform.component';
+import { TravelformComponent } from './travelform/travelform.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SignupformComponent } from './signupform/signupform.component';
     LoginformComponent,
     HomeComponent,
     UserprofileComponent,
-    SignupformComponent
+    SignupformComponent,
+    TravelformComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,7 @@ import { SignupformComponent } from './signupform/signupform.component';
     HttpModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [AuthService, IsLoggedInService],
+  providers: [AuthService, IsLoggedInService, AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/services/travel.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, EventEmitter } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Http } from "@angular/http";
+import "rxjs";
+import { environment } from "../../environments/environment";
+
+const BASEURL = environment.BASEURL + "/auth";
+
+@Injectable()
+export class AuthService {
+  private travel: object;
+  private options = { withCredentials: true };
+
+  constructor(private http: Http) {
+  }
+
+
+
+
+  private handleError(e) {
+    console.log("AUTH ERROR");
+    return Observable.throw(e.json().message);
+  }
+
+  
+
+  getTravels(origin, passengers, startDate, duration) {
+    console.log("entrooo");
+    return this.http
+      .post(`${BASEURL}/travels/search`, {origin, passengers, startDate, duration })
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+
+}
