829f08d0b1d93d06ded79887f5f3969cc9774961
Luisca108 <luiscarlos3106@gmail.com>
2017-10-12 16:28:18 +0200

    email imple

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require('path');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 var done = []
 
@@ -11,5 +12,32 @@ router.post("/search", (req, res, next) => {
         email: req.body.email
     }
     console.log(mail);
+
+    var text = 'Hello world from \n\n' + req.body.name;
+    // Not the movie transporter!
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: '	surprisetrip108@gmail.com', // Your email id
+            pass: 'ironhack12345' // Your password
+        }
+    });
+    var mailOptions = {
+        from: '	surprisetrip108@gmail.com', // sender address
+        to: req.body.email, // list of receivers
+        subject: 'Surprise!', // Subject line
+        text: text //, // plaintext body
+        // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.json({ yo: 'error' });
+        } else {
+            console.log('Message sent: ' + info.response);
+            res.json({ yo: info.response });
+        };
+    });
 });
 module.exports = router;
